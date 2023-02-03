965435df26eeeca9e31773bcbab76f60f453eb73
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-03 17:44:12 +0200

    Compile module

--- crates/cairo-lang-dojo/src/lib.rs
+++ crates/cairo-lang-dojo/src/lib.rs
@@ -3,6 +3,7 @@
 //! Dojo is a full stack toolchain for developing onchain games in Cairo.
 //!
 //! Learn more at [dojoengine.gg](http://dojoengine.gg).
+pub mod compiler;
 pub mod db;
 pub mod plugin;
 mod component;
