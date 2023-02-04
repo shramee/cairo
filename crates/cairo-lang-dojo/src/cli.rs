f61fd6ce8a93e02abc6becbc29cee8d050bcc0a6
Mathieu <60658558+msaug@users.noreply.github.com>
2023-02-04 11:30:17 +0200

    Feat/dojo plugin (#1)
    * refactor: import dojo plugin
    
    * feat: dojo compiler db plugin
    
    * feat: impl syscall/serde for Position
    
    * feat: add compile module (#2)
    
    * Compile module
    
    * Use compile module for CLI
    
    * Cleanup `use`es
    
    ---------
    
    Co-authored-by: Shramee Srivastav <shramee.srivastav@gmail.com>

---
+++ crates/cairo-lang-dojo/src/cli.rs
@@ -0,0 +1,37 @@
+use std::fs;
+use std::path::PathBuf;
+
+use anyhow::Context;
+use cairo_lang_dojo::compiler::compile_dojo_project_at_path;
+use clap::Parser;
+
+/// Command line args parser.
+/// Exits with 0/1 if the input is formatted correctly/incorrectly.
+#[derive(Parser, Debug)]
+#[clap(version, verbatim_doc_comment)]
+struct Args {
+    /// The file to compile
+    path: String,
+    /// The output file name (default: stdout).
+    output: Option<String>,
+    /// Replaces sierra ids with human readable ones.
+    #[arg(short, long, default_value_t = false)]
+    replace_ids: bool,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let path = &PathBuf::from(args.path);
+
+    let sierra_program = compile_dojo_project_at_path(path)?;
+
+    // let contract = compile_path(path, args.replace_ids)?;
+    match args.output {
+        Some(path) => {
+            fs::write(path, format!("{}", sierra_program)).context("Failed to write output.")?
+        }
+        None => println!("{}", sierra_program),
+    }
+
+    Ok(())
+}
---
+++ crates/cairo-lang-dojo/src/lib.rs
@@ -0,0 +1,11 @@
+//! Dojo capabilities and utilities on top of Starknet.
+//!
+//! Dojo is a full stack toolchain for developing onchain games in Cairo.
+//!
+//! Learn more at [dojoengine.gg](http://dojoengine.gg).
+pub mod compiler;
+pub mod db;
+pub mod plugin;
+mod component;
+mod system;
+mod query;
