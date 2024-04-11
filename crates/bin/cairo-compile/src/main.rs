598d06e8a986b233f27fea1ef62b39649492b8fa
cryptonerdcn <cryptonerdcn@gmail.com>
2024-04-12 00:56:03 +0900

    Add compile interface to the project     add_compile_interface(${CMAKE_CURRENT_SOURCE_DIR})

--- crates/bin/cairo-compile/src/main.rs
+++ crates/bin/cairo-compile/src/main.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 
 use anyhow::Context;
 use cairo_lang_compiler::project::check_compiler_path;
-use cairo_lang_compiler::{compile_cairo_project_at_path, CompilerConfig};
+use cairo_lang_compiler::{compile_cairo_project_at_path, CompilerConfig, wasm_cairo_interface::compile_cairo_project_with_input_string};
 use cairo_lang_utils::logging::init_logging;
 use clap::Parser;
 
@@ -22,6 +22,9 @@ struct Args {
     /// Replaces sierra ids with human-readable ones.
     #[arg(short, long, default_value_t = false)]
     replace_ids: bool,
+    /// Input program string of Cairo code.
+    #[arg(long)]
+    input_program_string: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -31,12 +34,19 @@ fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     // Check if args.path is a file or a directory.
-    check_compiler_path(args.single_file, &args.path)?;
-
-    let sierra_program = compile_cairo_project_at_path(
-        &args.path,
-        CompilerConfig { replace_ids: args.replace_ids, ..CompilerConfig::default() },
-    )?;
+    // check_compiler_path(args.single_file, &args.path)?;
+    
+    let sierra_program = match args.input_program_string {
+        Some(input_program_string) => compile_cairo_project_with_input_string(
+            &args.path,
+            &input_program_string,
+            CompilerConfig { replace_ids: args.replace_ids, ..CompilerConfig::default() },
+        )?,
+        None => compile_cairo_project_at_path(
+            &args.path,
+            CompilerConfig { replace_ids: args.replace_ids, ..CompilerConfig::default() },
+        )?,
+    };
 
     match args.output {
         Some(path) => {
